3b9cc65f7568d21c00510063bedca2eb21c888bb
Akshay Paunikar <akshayakki29@gmail.com>
2024-06-21 19:07:43 +0530

    revision & practice - Database Manipulation

---
+++ REVISION_&_PRACTICE/revision_&_practice.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS revision_practice;
+USE revision_practice;
+SHOW DATABASES;
+SELECT DATABASE(); -- Displays Current Database Selected
+CREATE DATABASE deletethis;
+DROP DATABASE IF EXISTS deletethis;
