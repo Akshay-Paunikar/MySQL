5a71107541a9dbd172ecec5c037ace4c0d0faf8d
Akshay Paunikar <akshayakki29@gmail.com>
2024-06-21 19:18:25 +0530

    revision & practice - Storage Engines

--- REVISION_&_PRACTICE/revision_&_practice.sql
+++ REVISION_&_PRACTICE/revision_&_practice.sql
@@ -6,3 +6,22 @@ SHOW DATABASES;
 SELECT DATABASE(); -- Displays Current Database Selected
 CREATE DATABASE deletethis;
 DROP DATABASE IF EXISTS deletethis;
+
+-- STORAGE ENGINES -- INNODB, MyISAM, MERGE (MRG_MyISAM), CSV 
+
+SET DEFAULT_STORAGE_ENGINE = INNODB;
+
+-- Setting default storage engine for a table --
+
+CREATE TABLE IF NOT EXISTS engine(
+emp_id VARCHAR(4) NOT NULL,
+first_name VARCHAR(100) NOT NULL,
+last_name VARCHAR(100),
+gender VARCHAR(1),
+role VARCHAR(100),
+dept VARCHAR(100),
+manager_id VARCHAR(4),
+CHECK(gender IN ('M', 'F', 'O')) 
+) ENGINE=INNODB;
+
+ALTER TABLE engine ENGINE = MERGE; -- For changing the storage engine of table 
