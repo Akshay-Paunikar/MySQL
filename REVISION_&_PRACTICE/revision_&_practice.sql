6b0d46efcdedfe71e6f1f9307af45a08a911b575
Akshay Paunikar <akshayakki29@gmail.com>
2024-06-21 19:41:14 +0530

    revision & practice - Create & Analyze Tables

--- REVISION_&_PRACTICE/revision_&_practice.sql
+++ REVISION_&_PRACTICE/revision_&_practice.sql
@@ -1,4 +1,6 @@
--- DATABASE MANIPULATION -- 
+---------------------------
+-- DATABASE MANIPULATION --
+--------------------------- 
 
 CREATE DATABASE IF NOT EXISTS revision_practice;
 USE revision_practice;
@@ -7,7 +9,10 @@ SELECT DATABASE(); -- Displays Current Database Selected
 CREATE DATABASE deletethis;
 DROP DATABASE IF EXISTS deletethis;
 
--- STORAGE ENGINES -- INNODB, MyISAM, MERGE (MRG_MyISAM), CSV 
+---------------------
+-- STORAGE ENGINES --
+---------------------
+-- INNODB, MyISAM, MERGE (MRG_MyISAM), CSV 
 
 SET DEFAULT_STORAGE_ENGINE = INNODB;
 
@@ -25,3 +30,51 @@ CHECK(gender IN ('M', 'F', 'O'))
 ) ENGINE=INNODB;
 
 ALTER TABLE engine ENGINE = MERGE; -- For changing the storage engine of table 
+
+--------------------------------
+-- CREATING & MANAGING TABLES --
+--------------------------------
+
+CREATE TABLE IF NOT EXISTS emp_records(
+emp_id VARCHAR(4) NOT NULL,
+first_name VARCHAR(100) NOT NULL,
+last_name VARCHAR(100),
+gender VARCHAR(1)
+)ENGINE = INNODB;
+
+SELECT * FROM engine;
+
+/* CHECK Constraints */
+
+CREATE TABLE IF NOT EXISTS emp_records_2(
+emp_id VARCHAR(4) NOT NULL,
+first_name VARCHAR(100) NOT NULL,
+last_name VARCHAR(100),
+gender VARCHAR(1),
+role VARCHAR(100),
+dept VARCHAR(100),
+manager_id VARCHAR(4),
+exp INTEGER NOT NULL CHECK(EXP >= 0) 
+) ENGINE=INNODB;
+
+SELECT * FROM revision_practice.emp_records_2;
+
+CREATE TABLE IF NOT EXISTS emp_records_3(
+emp_id VARCHAR(4) NOT NULL,
+first_name VARCHAR(100) NOT NULL,
+last_name VARCHAR(100),
+gender VARCHAR(1),
+role VARCHAR(100),
+dept VARCHAR(100),
+manager_id VARCHAR(4),
+exp INTEGER NOT NULL CHECK(EXP >= 0),
+CONSTRAINT gender_check CHECK(gender IN ('M', 'F', 'O'))
+) ENGINE=INNODB;
+
+SELECT * FROM emp_records_3;
+
+SHOW TABLES; -- List all the existing table in the database
+DESCRIBE emp_records; -- To analyze the structure of table
+DESCRIBE emp_records_2;
+DESCRIBE emp_records_3;
+
